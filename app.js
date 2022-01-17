59d5facb63c4a137cd74a1580462c677b8a8a460
ludchieng <lud.chieng@hotmail.fr>
2022-01-17 18:57:41 +0100

    Update

--- app.js
+++ app.js
@@ -1,60 +1,58 @@
 import express from 'express';
 import ejs from 'ejs';
+import fetch from 'node-fetch';
+
+// Medical sheets just as example
+import DUMB_SHEETS from './dumbdata.js';
 
 const app = express();
 const PORT = 8000;
 
-const data = {
-  name: "M. Gérard Durand",
-  dob: "25/04/1973 · 48 ans",
-  height: "178 cm",
-  weight: "86 kg",
-  blocks: [
-    {
-      name: "Antécédents médicaux",
-      content: "Hypertension artérielle",
-    },
-    {
-      name: "Allergies et réactions",
-      content: "Venin d'abeilles",
-    },
-    {
-      name: "Traitements en cours",
-      content: "Corticoïdes",
-    },
-    {
-      name: "Groupe sanguin",
-      content: "O+",
-    },
-  ],
-  contacts: [
-    {
-      role: "Compagne",
-      name: "Marie Durand",
-      content: "06 24 78 85 10",
-    },
-    {
-      role: "Frère",
-      name: "Fabrice Durand",
-      content: "06 85 96 22 05",
-    },
-  ]
-};
 
-app.get('/', function (req, res) {
-  ejs.renderFile('sostag.ejs', data,
-    {}, function (err, template) {
-      if (err) {
-        throw err;
-      } else {
-        res.end(template);
-      }
-    });
+app.get('/:qrcode', async (req, res) => {
+  console.log(`[GET] ${req.params.qrcode}`);
+
+  // Simulates API call to database
+  const r = await fetch(`http://127.0.0.1:${PORT}/data/${req.params.qrcode}`)
+
+  if (r.status === 200) {
+    const sheet = await r.json();
+    ejs.renderFile('sostag.ejs', sheet,
+      {}, (err, template) => {
+        if (err) {
+          throw err;
+        } else {
+          res.end(template);
+        }
+      });
+
+  } else if (r.status === 404) {
+    res.status(404).send('404: Fiche introuvable 🍉🌵');
+
+  } else {
+    res.status(500).send('500: March pas :\'(');
+  }
 });
 
-app.listen(PORT, function (error) {
+
+// Simulates API endpoint of the database
+app.get('/data/:qrcode', (req, res) => {
+  const sheet = DUMB_SHEETS[req.params.qrcode];
+  if (sheet)
+    res.json(sheet)
+  else
+    res.sendStatus(404);
+});
+
+
+app.get('/', (req, res) => {
+  res.status(404).send('404: Aucun QR Code spécifié 🍉🌵');
+});
+
+
+app.listen(PORT, (error) => {
   if (error)
     throw error;
   else
-    console.log("Server is running");
+    console.log("[---] Listening on port 8000");
 });
---
+++ dumbdata.js
@@ -0,0 +1,81 @@
+export default {
+  e8agr56z258gtoyi: {
+    name: "Mme Mélanie Castex",
+    dob: "05/11/1969 · 52 ans",
+    blocks: [
+      {
+        name: "Antécédents médicaux",
+        content: "Hypertension artérielle",
+      },
+      {
+        name: "Allergies et réactions",
+        content: "Polen",
+      },
+      {
+        name: "Traitements en cours",
+        content: "RAS",
+      },
+      {
+        name: "Groupe sanguin",
+        content: "AB+",
+      },
+      {
+        name: "Taille",
+        content: "168 cm",
+      },
+      {
+        name: "Poids",
+        content: "56 kg",
+      },
+    ],
+    contacts: [
+      {
+        role: "Frère",
+        name: "Martin Castex",
+        content: "07 12 32 00 52",
+      },
+    ]
+  },
+  pry5g2wfkjo562f5: {
+    name: "M. Gérard Durand",
+    dob: "25/04/1973 · 48 ans",
+    blocks: [
+      {
+        name: "Antécédents médicaux",
+        content: "Hypertension artérielle",
+      },
+      {
+        name: "Allergies et réactions",
+        content: "Venin d'abeilles",
+      },
+      {
+        name: "Traitements en cours",
+        content: "Corticoïdes",
+      },
+      {
+        name: "Groupe sanguin",
+        content: "O+",
+      },
+      {
+        name: "Taille",
+        content: "178 cm",
+      },
+      {
+        name: "Poids",
+        content: "86 kg",
+      },
+    ],
+    contacts: [
+      {
+        role: "Compagne",
+        name: "Marie Durand",
+        content: "06 24 78 85 10",
+      },
+      {
+        role: "Frère",
+        name: "Fabrice Durand",
+        content: "06 85 96 22 05",
+      },
+    ]
+  }
+};
