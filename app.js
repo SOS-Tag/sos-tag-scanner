6e28dbc4b5e672ce4f5ee61997efda5dbcd29d70
Alaric Rougnon-Glasson <alaric.rougnon.glasson@gmail.com>
2022-01-19 16:34:53 +0100

    [getAPIData] Add routes for unknown id + refactor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import ejs from 'ejs';
+import express, { response } from 'express';
+import ejs, { Template } from 'ejs';
 import fetch from 'node-fetch'
 
 const app = express();
@@ -52,11 +52,13 @@ app.get('/', function(req, res) {
     });
 });
 
-// Route to get user health sheet by id
+/****************************************/
+/* ROUTE TO GET USER HEALTH SHEET BY ID */
+/****************************************/
 // example : http://localhost:8000/123456789
 // where 123456789 is the user id
-app.get('/:id', (req, res) => {
 
+app.get('/:id', async (req, res) => {
   // Create graphql request to API
   const apiReq = JSON.stringify({
     query: `query Query($userId: String) {
@@ -68,7 +70,6 @@ app.get('/:id', (req, res) => {
           response {
             firstname
             lastname
-            _id
           }
         }
       }`,
@@ -77,32 +78,73 @@ app.get('/:id', (req, res) => {
     }
   })
 
-  // Get dat from API (using graphql)
-  fetch('http://localhost:8080/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: apiReq
-  })
-    .then(res => res.json())
-    .then(res => res.data)
-    .then(data => {
-      // TEMPORARY
-      console.log('data : ', data)
-      const finalData = { ...fakeData, name: data.userById.response.firstname + " " + data.userById.response.lastname }
-      console.log('finalData : ', finalData)
-
-      // render health sheet with data
-      ejs.renderFile('sostag.ejs', finalData, {}, (err, template) => {
-        if (err) {
-          throw err
-        } else {
-          res.end(template)
-        }
-      })
+  const apiRes = await fetch(
+    'http://localhost:8080/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: apiReq
     })
+
+  // handle request error
+  if (!apiRes.ok) throw new Error('Error occured : request to sos-tag graphql API failed')
+
+  let data = await apiRes.json()
+  data = data.data
+  console.log('data : ', data)
+
+  if (data.userById) {
+    // Render healt sheet template filled with user data
+    const finalData = { ...fakeData, name: data.userById.response.firstname + " " + data.userById.response.lastname }
+    console.log('finalData : ', finalData)
+
+    ejs.renderFile('templates/sostag.ejs', finalData, {}, (err, template) => {
+      if (err) {
+        throw err
+      } else {
+        res.end(template)
+      }
+    })
+  } else {
+    ejs.renderFile('templates/healthSheetNotFound.ejs', { id: req.params.id }, {}, (err, template) => {
+      if (err) {
+        throw err
+      } else {
+        res.end(template)
+      }
+    })
+  }
 })
+
+// Get dat from API (using graphql)
+//   fetch('http://localhost:8080/graphql', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: apiReq
+//   })
+//     .then(res => res.json())
+//     .then(res => res.data)
+//     .then(data => {
+//       // TEMPORARY
+//       console.log('data : ', data)
+//       const finalData = { ...fakeData, name: data.userById.response.firstname + " " + data.userById.response.lastname }
+//       console.log('finalData : ', finalData)
+//
+//       // render health sheet with data
+//       ejs.renderFile('templates/sostag.ejs', finalData, {}, (err, template) => {
+//         if (err) {
+//           throw err
+//         } else {
+//           res.end(template)
+//         }
+//       })
+//     })
+//     .catch(err)
+// })
 
 app.listen(PORT, function(error) {
   if (error)
